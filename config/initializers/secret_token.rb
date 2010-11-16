343cf5b2c800279cfa859ac42b279e444c6558e4
unknown <ehagen@.(none)>
2010-11-16 00:25:42 -0800

    Sekelton Rails 3.0.1 app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RwbGithub::Application.config.secret_token = '6608296cea54e0ed393b4262d860c1ac9fabcb109e7caa8f4b5db9d10deb3a9c8af3dcff35851d17f441fa963d17a243f627cbba87de413298ff4f30fc79cf5c'

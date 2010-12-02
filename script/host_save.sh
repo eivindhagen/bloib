218a9c155b553ccafd3343bc99a4a07e2be509b0
ehagen <ehagen@working-progress.com>
2010-12-01 23:45:31 -0800

    Mostly cosmetic updates to host_save and host_scan scripts, and gave them proper file extensions so that editors know how to handle them.

---
+++ script/host_save.sh
@@ -0,0 +1,29 @@
+# saves a copy of a host configuration file in a parallel directory structure (cloneDir), for safe keeping and future reference.
+
+# echo all args: $*
+# echo arg 0: $0
+# echo num args: "$#"
+
+if [ $# != 2 ]; then
+    echo "Usage: $0 <cloneDir> <file>"
+	echo BTW, your host is: `cat /etc/*release`
+	exit 1;
+fi
+
+cloneDir=$1
+# echo "Verifying that $cloneDir exist"
+
+if [ ! -d $cloneDir ]; then
+    echo "Dir '$cloneDir' not found"
+	exit 1;
+fi
+
+file=$2
+if [ ! -e $file ]; then
+    echo "File '$file' not found"
+	exit 1;
+fi
+
+fileDir=$(dirname $file)
+mkdir -p $cloneDir.$fileDir
+cp -R -P $file $cloneDir.$file
---
+++ script/host_scan.rb
@@ -0,0 +1,92 @@
+#!/usr/bin/env ruby
+require 'find'
+
+def host_scan(cloneDir, rootDir)
+  diffNum = 0
+  missNum = 0
+  Find.find(cloneDir) do |path|
+    unless FileTest.directory?(path)
+      fileRelative = path[cloneDir.size..path.size]
+      fileOnRealHost = rootDir + fileRelative
+      fileInHostClone = cloneDir + fileRelative
+      if FileTest.exists?(fileOnRealHost )
+        diffCmd = "diff #{fileOnRealHost} #{fileInHostClone}"
+        diffResult = `#{diffCmd}`
+        if diffResult.size > 0
+          diffNum += 1
+          puts "!CHANGED!: #{fileOnRealHost}"
+          case option
+          when "test"
+          when "backup"
+            cmd = "cp #{fileOnRealHost} #{fileInHostClone}"
+            result = `#{cmd}`
+            puts result if result.size > 0
+          when "restore"
+            puts "restore to real host"
+          end
+        else
+          #puts "identical: #{fileOnRealHost}"
+        end
+      else
+        missNum += 1
+        puts "!MISSING!: #{fileOnRealHost}"
+          case option
+          when "test"
+          when "backup"
+            cmd = "rm #{fileInHostClone}"
+            result = `#{cmd}`
+            puts result if result.size > 0
+          when "restore"
+            puts "restore to real host"
+          end
+      end
+    end
+  end
+  return [diffNum, missNum]
+end
+
+
+if __FILE__ == $0
+  badArgs = false
+  
+  unless [1,2].include? ARGV.size
+    badArgs = true
+  end
+  
+  unless badArgs
+  	cloneDir = ARGV[0]
+  	
+  	unless cloneDir[cloneDir.size-1] == '/'
+  	  puts "<cloneDir> should have a trailing /"
+  	  puts "You called me with <cloneDir> = '#{cloneDir}'"
+  	  badArgs = true
+  	end
+  	
+  	option = "test" # default is to test to see which files are different, but not copy anything
+  	if ARGV.size == 2
+  	  option = ARGV[1]
+  	  unless ["test", "backup", "restore"].include? option
+  	    puts "Unknown option '#{option}'"
+  	    badArgs = true
+  	  end
+  	end
+  end
+  
+  if badArgs
+    puts "host_scan: scans cloneDir folder and compares all files with the matching files from the root of the real host."
+    puts "options:"
+    puts "  [test] will just check for differences"
+    puts "  [backup] will copy changed files into cloneDir, and delete files in cloneDir that do not exist on the real host"
+    puts "  [restore] should copy all changed or missing files from cloneDir to the real host (not implemented yet)"
+    puts "Usage: #{$0} <cloneDir> [test|backup|restore]"
+    puts "You called me with these args:"
+    for i in 0...(ARGV.size) do
+      puts "ARGV[#{i}]: #{ARGV[i]}"
+    end
+    exit 1
+  end
+  
+  puts "Scanning #{cloneDir}"
+  diffNum, missNum = host_scan(cloneDir, "/")
+  puts "Summary: changed=#{diffNum} missing=#{missNum}"
+end

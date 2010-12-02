a04ff848aadf98798f00f179f705f58b52e12bb2
ehagen <ehagen@working-progress.com>
2010-12-02 01:02:42 -0800

    Fixed another typo

--- app/controllers/demo/host_scan_controller.rb
+++ app/controllers/demo/host_scan_controller.rb
@@ -1,7 +1,7 @@
 class Demo::HostScanController < ApplicationController
   def index
     appDir = ::Rails.root.to_s
-    @cmd = "ruby #{appDir}/script/host_scan.rb #{appDir}/hosts/Ubuntu-10.04 --test"
+    @cmd = "ruby #{appDir}/script/host_scan.rb #{appDir}/hosts/Ubuntu-10.04/ --test"
     proc = IO.popen(@cmd)
     @pid = proc.pid
     @result = proc.readlines.join

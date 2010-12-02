66a5c69e54928fc7bd5616f96e08845de749c166
ehagen <ehagen@working-progress.com>
2010-12-02 00:49:07 -0800

    demo/host_scan - now running the script for real

--- app/controllers/demo/host_scan_controller.rb
+++ app/controllers/demo/host_scan_controller.rb
@@ -2,10 +2,9 @@ class Demo::HostScanController < ApplicationController
   def index
     appDir = ::Rails.root.to_s
     @cmd = "ruby #{appDir}/scripts/host_scan.rb #{appDir}/hosts/Ubuntu-10.04 --test"
-    @cmd = "echo #{appDir}/scripts/host_scan.rb"
     proc = IO.popen(@cmd)
     @pid = proc.pid
-    @result = "R" + proc.readlines.join
+    @result = proc.readlines.join
     Process.waitpid(@pid)
     @status = $?
   end

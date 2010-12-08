0b96b765fa76467df5cfb20cc76127f25e968600
unknown <ehagen@.(none)>
2010-12-08 02:37:51 -0800

    Changed cookie store key name

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-RwbGithub::Application.config.session_store :cookie_store, :key => '_rwb_github_session'
+RwbGithub::Application.config.session_store :cookie_store, :key => '_rwb_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
